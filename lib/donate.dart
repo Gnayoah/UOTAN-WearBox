bc1bace756f0f5fb34a8c659ccf833449ca76503
Gnayoah <haoyang@gnayoah.com>
2024-09-17 18:50:25 -0700

    feat: 捐赠前检查连网

--- lib/donate.dart
+++ lib/donate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:http/http.dart' as http;
 
 class DonatePage extends StatefulWidget {
   const DonatePage({super.key});
@@ -36,6 +37,53 @@ class _DonatePageState extends State<DonatePage> with SingleTickerProviderStateM
         _updateQrCodeUrl(); // 更新二维码URL
       });
     });
+
+    // 检查网络连接
+    _checkNetworkConnection();
+  }
+
+  Future<void> _checkNetworkConnection() async {
+    try {
+      final response = await http.get(Uri.parse('https://wear.gnayoah.com'));
+      if (response.statusCode != 200) {
+        _showNoNetworkDialog();
+      }
+    } catch (e) {
+      _showNoNetworkDialog();
+    }
+  }
+
+  void _showNoNetworkDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xFFF9F9F9), // 弹窗背景颜色
+          title: const Text('网络连接错误'),
+          content: const Text('无法连接服务器，请检查您的网络连接。'),
+          actions: [
+            TextButton(
+              style: ButtonStyle(
+                overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed) || states.contains(MaterialState.hovered)) {
+                      return const Color.fromARGB(255, 237, 237, 237); // 点击或悬浮时的背景颜色
+                    }
+                    return null; // 默认状态下不更改颜色
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.black), // 文本颜色
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pop(context); // 返回到上一个页面
+              },
+              child: const Text('确认', style: TextStyle(color: Colors.black)),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _updateQrCodeUrl() {
@@ -72,7 +120,7 @@ class _DonatePageState extends State<DonatePage> with SingleTickerProviderStateM
                       ),
                       const SizedBox(width: 5),
                       const Text(
-                        '支持我们', // 页面标题
+                        '支持我们 (❁´◡`❁)', // 页面标题
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 26,
@@ -86,72 +134,71 @@ class _DonatePageState extends State<DonatePage> with SingleTickerProviderStateM
             ),
           ),
         ),
-        
       ),
-      
       body: Padding(
-        
         padding: const EdgeInsets.all(25.0), // 设置页面内边距
-        
         child: Row(
-          
           children: [
-           
             Expanded(
-              
               flex: 2, // 设置按钮部分所占比例
-              child: GridView.count(
-                crossAxisCount: 2, // 两列
-                crossAxisSpacing: 10, // 列间距
-                mainAxisSpacing: 10, // 行间距
-                childAspectRatio: 3, // 按钮的宽高比，设置为较宽的比例
-                children: qrCodeUrls.keys.map((buttonLabel) {
-                    
-                  return Card(
-                    
-                    color: const Color(0xFFF9F9F9), // 设置卡片背景颜色
-                    elevation: 0, // 去掉阴影
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0), // 圆角效果
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 25),
+                  const Text(
+                    '选择金额：',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 20), // 添加一些间距
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2, // 两列
+                      crossAxisSpacing: 10, // 列间距
+                      mainAxisSpacing: 10, // 行间距
+                      childAspectRatio: 3, // 按钮的宽高比，设置为较宽的比例
+                      children: qrCodeUrls.keys.map((buttonLabel) {
+                        return Card(
+                          color: const Color(0xFFF9F9F9), // 设置卡片背景颜色
+                          elevation: 0, // 去掉阴影
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0), // 圆角效果
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              setState(() {
+                                selectedAmount = buttonLabel; // 更新选中的金额
+                                _updateQrCodeUrl(); // 更新二维码URL
+                              });
+                            },
+                            child: Center(
+                              child: Text(
+                                buttonLabel,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          ),
+                        );
+                      }).toList(),
                     ),
-                    child: InkWell(
-                      onTap: () {
-                        setState(() {
-                          selectedAmount = buttonLabel; // 更新选中的金额
-                          _updateQrCodeUrl(); // 更新二维码URL
-                        });
-                      },
-                      
-                      child: Center(
-                        
-                        child: Text(
-                          buttonLabel,
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
+                  ),
+                ],
               ),
             ),
             const SizedBox(width: 20), // 左右部分的间距
             Expanded(
               flex: 1, // 设置二维码部分所占比例
               child: Column(
-                
                 children: [
-                   
+                  const SizedBox(height: 55),
                   Container(
                     width: 180,
-                  child: Card(
-                    
-                    color: const Color(0xFFF9F9F9), // 设置卡片背景颜色
-                    elevation: 0, // 去掉阴影
-                    
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0), // 圆角效果
-                    ),
-                   child: InkWell(
+                    child: Card(
+                      color: const Color(0xFFF9F9F9), // 设置卡片背景颜色
+                      elevation: 0, // 去掉阴影
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0), // 圆角效果
+                      ),
+                      child: InkWell(
                         child: Center(
                           child: Container(
                             padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0), // 添加上下和左右的内边距
@@ -170,40 +217,36 @@ class _DonatePageState extends State<DonatePage> with SingleTickerProviderStateM
                               labelColor: Colors.black,
                               unselectedLabelColor: Colors.grey,
                               indicatorColor: Colors.transparent, // 去除下划线
-                              dividerHeight: 0,
-    tabs: [
-      Container(
-        height: 26, // 设置固定高度
-        alignment: Alignment.center,
-        child: const Text('  支付宝  '),
-      ),
-      Container(
-        height: 26, // 设置固定高度
-        alignment: Alignment.center,
-        child: const Text('  微信  '),
-      ),
-    ],
-  ),
-),
-
+                              dividerColor: Colors.transparent, // 去除分割线
+                              tabs: [
+                                Container(
+                                  height: 26, // 设置固定高度
+                                  alignment: Alignment.center,
+                                  child: const Text(' 支付宝 '),
+                                ),
+                                Container(
+                                  height: 26, // 设置固定高度
+                                  alignment: Alignment.center,
+                                  child: const Text(' 微信 '),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  ),
-                  // 将 TabBar 移动到二维码上方并自定义样式
-                 
-                  const SizedBox(height: 10), // 添加一些间距
+                  const SizedBox(height: 12), // 添加一些间距
                   Stack(
                     alignment: Alignment.center,
                     children: [
                       const CircularProgressIndicator(
-                         color: Colors.black, // 进度条颜色设置为黑色
+                        color: Colors.black, // 进度条颜色设置为黑色
                       ), // 在加载图片时显示进度条
-
                       Image.network(
                         currentQrCodeUrl, // 根据按钮点击事件更换二维码图片
-                        width: 200, // 固定宽度
-                        height: 200, // 固定高度，确保是正方形
+                        width: 155, // 固定宽度
+                        height: 155, // 固定高度，确保是正方形
                         fit: BoxFit.cover,
                         loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
                           if (loadingProgress == null) {
@@ -215,9 +258,9 @@ class _DonatePageState extends State<DonatePage> with SingleTickerProviderStateM
                       ),
                     ],
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 13),
                   Text(
-                    '￥${selectedAmount.replaceAll('元', '')}',
+                    '已选捐赠金额 - ￥${selectedAmount.replaceAll('元', '')}',
                     style: const TextStyle(fontSize: 18),
                   ),
                 ],
